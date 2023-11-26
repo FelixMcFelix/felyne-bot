f0fee8705a699b4d44ca1bd553aa807053df43a4
Kyle Simpson <kyleandrew.simpson@gmail.com>
2023-11-26 00:05:39 +0000

    Symphonia doesn't support ATRAC3

--- src/constants.rs
+++ src/constants.rs
@@ -30,19 +30,19 @@ pub const SFX: &[&str] = &[
 
 pub const BONUS_SFX: &[&str] = &["gargwa1.opus", "gargwa2.opus", "gargwa3.opus"];
 
-pub const BBQ: &[&str] = &["5796.wav"];
+pub const BBQ: &[&str] = &["5796.opus"];
 
-pub const BBQ_RESULT: &[&str] = &["5797.wav", "5798.wav"];
+pub const BBQ_RESULT: &[&str] = &["5797.opus", "5798.opus"];
 
-pub const SLEEP: &[&str] = &["5799.wav"];
+pub const SLEEP: &[&str] = &["5799.opus"];
 
-pub const START: &[&str] = &["5839.wav", "5840.wav", "5841.wav", "5842.wav"];
+pub const START: &[&str] = &["5839.opus", "5840.opus", "5841.opus", "5842.opus"];
 
 pub const AMBIENCE: &[&str] = &[
-	"5824.wav", "5825.wav", "5826.wav", "5827.wav", "5828.wav", "5829.wav", "5830.wav", "5831.wav",
-	"5832.wav",
+	"5824.opus", "5825.opus", "5826.opus", "5827.opus", "5828.opus", "5829.opus", "5830.opus", "5831.opus",
+	"5832.opus",
 ];
 
-pub const BGM: &[&str] = &["5815.wav", "5816.wav", "5817.wav", "6383.wav", "6384.wav"];
+pub const BGM: &[&str] = &["5815.opus", "5816.opus", "5817.opus", "6383.opus", "6384.opus"];
 
 pub const TRACE_DIR: &str = "traces/";
